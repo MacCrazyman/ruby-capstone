fa7d3a2a4b61640a4187c16d65823a79b1f7c063
Diego Solis <disc3110@gmail.com>
2022-03-14 14:10:11 -0600

    Add methods for item class

--- classes/Item.rb
+++ classes/Item.rb
@@ -6,4 +6,21 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+
+  def add_genre
+  end
+
+  def add_label
+  end
+
+  def add_author
+  end
+
+  def move_to_archive
+  end
+
+  private
+
+  def can_be_archived?
+  end
 end
