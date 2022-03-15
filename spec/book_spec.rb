f6cb09a451ee4d54a64ee4c89875359e1645f2e1
Diego Solis <disc3110@gmail.com>
2022-03-15 10:49:14 -0600

    add tests for can_be_archived? method in Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,4 +11,17 @@ describe Book do
   it 'creates an instance of an Item' do
     expect(@book).to be_a Item
   end
+  describe '#can_be_archived?' do
+    it 'should return true if cover_state equals to "bad"' do
+      bad_cover_book = Book.new('Oxford', 'bad', '2020-10-09')
+      expect(bad_cover_book.can_be_archived?).to be true
+    end
+    it 'should return true if parents method returns true' do
+      old_book = Book.new('Oxford', 'good', '1985-10-09')
+      expect(old_book.can_be_archived?).to be true
+    end
+    it 'otherwise should return false' do
+      expect(@book.can_be_archived?).to be false
+    end
+  end
 end
