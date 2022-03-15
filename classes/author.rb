fe88f47506bbc9834da3cf0a5c51d5d71dd3e5d6
Lucas-Masaba <masabaluke@gmail.com>
2022-03-15 19:05:11 +0300

    add methods to the author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,8 +1,17 @@
+require_relative './item'
 class Author
   attr_accessor :first_name, :last_name
+  attr_reader :items
 
-  def initialize(_first_name, _last_name)
+  def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
     @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
   end
 end
