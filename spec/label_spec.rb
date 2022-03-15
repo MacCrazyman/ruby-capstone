d9d862bb86a6bf215c9791a9ba3e7cdd9b893742
Diego Solis <disc3110@gmail.com>
2022-03-14 22:07:03 -0600

    add tests for add_item method of label

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../classes/label'
+require_relative '../classes/item'
 
 describe Label do
   before :each do
@@ -7,5 +8,23 @@ describe Label do
 
   it 'creates an instance of Label' do
     expect(@label).to be_a Label
+  end
+
+  describe '#add_item' do
+    it 'can just take an instance of the item class' do
+      expect { @label.add_item('hdcn') }.to raise_error(ArgumentError, 'item must be of Item class')
+    end
+
+    it 'should add the input item to the collection of items' do
+      item = Item.new('2020-10-19')
+      @label.add_item(item)
+      expect(@label.items.length).to eq 1
+    end
+
+    it 'should add self as a property of the item object' do
+      item = Item.new('2020-10-19')
+      @label.add_item(item)
+      expect(item.label).to eq @label
+    end
   end
 end
