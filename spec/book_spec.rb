54bb2db838a07bf91a774850555045994b2b05a5
Diego Solis <disc3110@gmail.com>
2022-03-14 18:40:40 -0600

    Add tests for book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/book'
+
+describe Book do
+  before :each do
+    @book = Book.new('Oxford University Press', 'good', '2020-10-25')
+  end
+  it 'creates an instance of a Book' do
+    expect(@book).to be_a Book
+  end
+  it 'creates an instance of an Item' do
+    expect(@book).to be_a Item
+  end
+end
