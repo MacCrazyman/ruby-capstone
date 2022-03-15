9eab31e006d4cfa43ad1f55be3b88abc6a73f930
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-03-15 10:01:33 -0500

    Add on_spotify property

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,7 +1,8 @@
 require_relative './item'
-
 class MusicAlbum < Item
+  attr_accessor :on_spotify
   def initialize(*args)
     super(*args)
+    @on_spotify = false
   end
 end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -8,4 +8,15 @@ describe 'MusicAlbum' do
   it 'should be a MusicAlbum instance' do
     @album.should be_an_instance_of MusicAlbum
   end
+
+  it 'should return the on_spotify property' do
+    expect(@album.on_spotify).to be false
+  end
+
+  it 'can be archived' do
+    archived_before = @album.archived
+    @album.move_to_archive
+    expect(@album.archived).not_to be archived_before
+  end
+
 end
