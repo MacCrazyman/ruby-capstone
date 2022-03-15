d5f9f1b02b400e72209f0baa9fa7a1c2ddfb82d4
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-03-15 17:50:19 -0500

    Add some documentation

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,6 +1,13 @@
 require_relative './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
+  # class: Music Album class.
+  #
+  # *args  - Arguments from `item` parent
+  #
+  # Examples
+  #   album_1 = MusicAlbum.new('2022-04-04')
+  #   # => {Musicalbum}
   def initialize(*args)
     super(*args)
     @on_spotify = false
