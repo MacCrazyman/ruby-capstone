176edf1ba530ff73251d9be0885eba643d29b14e
Lucas-Masaba <masabaluke@gmail.com>
2022-03-16 23:05:56 +0300

    add storage to game and author

--- storage/input.rb
+++ storage/input.rb
@@ -8,7 +8,7 @@ class Input
   end
 
   def read_games(state)
-    games_json = File.open("#{@path}/games.json")
+    games_json = File.read("#{@path}/games.json")
     games_hash = JSON.parse(games_json)
     games = games_hash.map do |game|
       Game.new(game['multiplayer'], game['last_played_at'], game['publish_date'], game['id'])
